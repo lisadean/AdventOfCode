6ab05e8b9bb2f2fe7185546affe952fc41bab18a
Lisa Dean <lisadean@users.noreply.github.com>
2021-12-02 15:22:13 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+function part1() {
+  console.log('===== PART ONE =====')
+  let increased = 0;
+  let previous;
+  const allFileContents = fs.readFileSync('input-1-1.txt', 'utf-8');
+  allFileContents.split(/\r?\n/).forEach((line, idx) =>  {
+    const newDepth = Number(line);
+    if(idx < 4) console.log(`Previous: ${previous}, newDepth: ${newDepth}`)
+    if (previous && newDepth > previous) {
+      increased++;
+    }
+    previous = newDepth;
+  });
+  console.log(`Total increased: ${increased}`)
+}
+
+function part2() {
+  console.log(`\n===== PART TWO =====`)
+  let increased = 0;
+  let previous;
+  let window = [];
+  const allFileContents = fs.readFileSync('input-1-1.txt', 'utf-8').split(/\r?\n/);
+  allFileContents.forEach((line, idx) =>  {
+    const newDepth = Number(line);
+    if(newDepth > 0) {
+      if(window.length === 3) {
+        window.shift();
+      }
+      window.push(newDepth);
+      if(idx > allFileContents.length - 4) console.log(window)
+      if(window.length === 3) {
+        const newWindowValue = window.reduce((a,b) => a + b);
+        const logLine = `previous: ${previous}, newWindowValue: ${newWindowValue}`
+        if((idx < 6) || (idx > allFileContents.length - 4)) console.log(logLine);
+        if(idx === 6) console.log(`...`);
+        if (previous && newWindowValue > previous) increased++;
+        previous = newWindowValue;
+      }
+    }
+  });
+  console.log(`Total increased: ${increased}`)
+}
+
+part1();
+part2();
