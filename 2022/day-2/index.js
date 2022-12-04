18dbec88cffc241c7494122d592373cfd72246f3
Lisa Dean <lisadean@users.noreply.github.com>
2022-12-04 21:13:07 +0000

    day 2 part 2 done

--- 2022/day-2/index.js
+++ 2022/day-2/index.js
@@ -1,15 +1,15 @@
 import { readFile } from 'fs/promises';
-const inputFile = 'sample.txt'; // part 1 = 15
-// const inputFile = 'input.txt'; // part 1 = 8392
+// const inputFile = 'sample.txt'; // part 1 = 15, part 2 = 12
+const inputFile = 'input.txt'; // part 1 = 8392, part 2 = 10116
 
 const input = (await readFile(inputFile, { encoding: 'utf8' }))
   .split('\n')
   .map((x) => x.split(' '));
 
 const itemValue = {
-  X: 1, // rock
-  Y: 2, // paper
-  Z: 3, // scissors
+  rock: 1,
+  paper: 2,
+  scissors: 3,
 };
 
 const rules = {
@@ -57,18 +57,54 @@ const part1 = async () => {
       roundResult = 'lose';
     }
     roundScore += resultValue[roundResult];
-    roundScore += itemValue[me];
+    roundScore += itemValue[itemDescription[me]];
     totalScore += roundScore;
-    console.log(
-      line,
-      roundResult,
-      resultValue[roundResult],
-      '+',
-      itemValue[me],
-      roundScore,
-      totalScore
-    );
+    // console.log(
+    //   line,
+    //   roundResult,
+    //   resultValue[roundResult],
+    //   '+',
+    //   itemValue[itemDescription[me]],
+    //   roundScore,
+    //   totalScore
+    // );
+  });
+  console.log(totalScore);
+};
+const part2 = async () => {
+  const resultMap = {
+    X: 'lose',
+    Y: 'draw',
+    Z: 'win',
+  };
+
+  let totalScore = 0;
+
+  input.forEach((line) => {
+    let myPlay,
+      roundScore = 0;
+    const [opponentPlay, result] = line;
+    if (resultMap[result] === 'draw') {
+      myPlay = itemDescription[opponentPlay];
+    } else if (resultMap[result] === 'win') {
+      myPlay = rules[itemDescription[opponentPlay]].losesTo;
+    } else {
+      myPlay = rules[itemDescription[opponentPlay]].beats;
+    }
+    roundScore += resultValue[resultMap[result]];
+    roundScore += itemValue[myPlay];
+    totalScore += roundScore;
+    // console.log(
+    //   line,
+    //   resultMap[result],
+    //   resultValue[resultMap[result]],
+    //   '+',
+    //   itemValue[myPlay],
+    //   roundScore,
+    //   totalScore
+    // );
   });
   console.log(totalScore);
 };
 part1();
+part2();
